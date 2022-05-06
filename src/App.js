cb121dabed6b9f68d1d7329f958e1db21ea7d441
Eric Wätke <eric.waetke@fh-potsdam.de>
2022-05-06 13:42:45 +0200

    :sparkles: Dragging Capabilities

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Draggable from 'react-draggable'; // The default
+import TEN_C from './assets/cards/10C';
+
 function App() {
+
+  const handleCardDrag = (e, data) => {
+    console.log(data);
+  }
+
+  const checkCollision = (collider, colliding) => {
+    const colliderRect = collider.getBoundingClientRect();
+    const collidingRect = colliding.getBoundingClientRect();
+    return !(
+      colliderRect.right < collidingRect.left ||
+      collidingRect.right < collidingRect.left ||
+      collidingRect.bottom < colliderRect.top ||
+      colliderRect.bottom < colliderRect.top
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='dragArea' style={{width: '500px', height: '500px', border: '1px solid white'}}></div>
+        <TEN_C />
       </header>
     </div>
   );
---
+++ src/assets/cards/10C.js
@@ -0,0 +1,141 @@
+import React, { useRef, useState } from "react";
+import Draggable from "react-draggable";
+import { ReactDOM } from "react";
+
+function TEN_C() {
+
+    const nodeRef = useRef(null)
+
+    const [activeDrags, setActiceDrags] = useState(0)
+    const [deltaPosition, setDeltaPosition] = useState({x: 0, y: 0})
+    const [controlledPosition, setControlledPosition] = useState({x: 0, y: 0})
+
+    const getViewportPosition = (e) => {
+        const boundingClientRect = nodeRef.current.getBoundingClientRect()
+        const x = boundingClientRect.left
+        const y = boundingClientRect.top
+        return { x, y }
+    }
+    
+    const handleStart = () => {
+    
+    }
+    const handleDrag = (e, ui) => {
+        const {x, y} = deltaPosition
+        setDeltaPosition(
+            {
+                x: x + ui.deltaX,
+                y: y + ui.deltaY
+            }
+        )
+        console.log(`x: ${x} -- y: ${y}`)
+        console.log(getViewportPosition())
+    }
+    const handleStop = (e) => {
+    
+    }
+  return (
+    <Draggable
+    nodeRef={nodeRef}
+    defaultPosition={{x: 0, y: 0}}
+    position={null}
+    // grid={[25, 25]}
+    scale={1}
+    onStart={handleStart}
+    onDrag={handleDrag}
+    onStop={handleStop}>
+    <div ref={nodeRef}>
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="240"
+        height="336"
+        className="card"
+        preserveAspectRatio="none"
+        viewBox="-120 -168 240 336"
+        >
+        <symbol
+            id="C"
+            preserveAspectRatio="xMinYMid"
+            viewBox="-600 -600 1200 1200"
+        >
+            <path d="M30 150c5 235 55 250 100 350h-260c45-100 95-115 100-350a10 10 0 00-20 0 210 210 0 11-74-201 10 10 0 0014-14 230 230 0 11220 0 10 10 0 0014 14 210 210 0 11-74 201 10 10 0 00-20 0z"></path>
+        </symbol>
+        <symbol
+            id="CT"
+            preserveAspectRatio="xMinYMid"
+            viewBox="-500 -500 1000 1000"
+        >
+            <path
+            fill="none"
+            stroke="#000"
+            strokeLinecap="square"
+            strokeMiterlimit="1.5"
+            strokeWidth="80"
+            d="M-260 430v-860M-50 0v-310a150 150 0 01300 0v620a150 150 0 01-300 0z"
+            ></path>
+        </symbol>
+        <rect
+            width="239"
+            height="335"
+            x="-119.5"
+            y="-167.5"
+            fill="#fff"
+            stroke="#000"
+            rx="12"
+            ry="12"
+        ></rect>
+        <use width="32" height="32" x="-114.4" y="-156" xlinkHref="#CT"></use>
+        <use
+            width="26.769"
+            height="26.769"
+            x="-111.784"
+            y="-119"
+            xlinkHref="#C"
+        ></use>
+        <use width="70" height="70" x="-87.501" y="-135.588" xlinkHref="#C"></use>
+        <use width="70" height="70" x="17.501" y="-135.588" xlinkHref="#C"></use>
+        <use width="70" height="70" x="-87.501" y="-68.529" xlinkHref="#C"></use>
+        <use width="70" height="70" x="17.501" y="-68.529" xlinkHref="#C"></use>
+        <use width="70" height="70" x="-35" y="-102.058" xlinkHref="#C"></use>
+        <g transform="rotate(180)">
+            <use width="32" height="32" x="-114.4" y="-156" xlinkHref="#CT"></use>
+            <use
+            width="26.769"
+            height="26.769"
+            x="-111.784"
+            y="-119"
+            xlinkHref="#C"
+            ></use>
+            <use
+            width="70"
+            height="70"
+            x="-87.501"
+            y="-135.588"
+            xlinkHref="#C"
+            ></use>
+            <use
+            width="70"
+            height="70"
+            x="17.501"
+            y="-135.588"
+            xlinkHref="#C"
+            ></use>
+            <use
+            width="70"
+            height="70"
+            x="-87.501"
+            y="-68.529"
+            xlinkHref="#C"
+            ></use>
+            <use width="70" height="70" x="17.501" y="-68.529" xlinkHref="#C"></use>
+            <use width="70" height="70" x="-35" y="-102.058" xlinkHref="#C"></use>
+        </g>
+        </svg>
+    </div>
+  </Draggable>
+
+  );
+}
+
+export default TEN_C;
